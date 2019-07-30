7aa9c34cabcbaf590034a449c9a490d46ffbafcd
akhilmantha <akhilmantha@live.com>
2019-07-30 14:48:45 +0530

    minor changes to dbsetup

--- python-pusher-traffice-monitor/dbsetup.py
+++ python-pusher-traffice-monitor/dbsetup.py
@@ -1,7 +1,7 @@
 import sqlite3
-    from sqlite3 import Error
+from sqlite3 import Error
 
-    def create_connection(database):
+def create_connection(database):
         try:
             conn = sqlite3.connect(database, isolation_level=None, check_same_thread = False)
             conn.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
@@ -10,48 +10,48 @@ import sqlite3
         except Error as e:
             print(e)
 
-    def create_table(c,sql):
-        c.execute(sql)
+def create_table(c,sql):
+    c.execute(sql)
 
-    def update_or_create_page(c,data):
-        sql = "SELECT * FROM pages where name=? and session=?"
-        c.execute(sql,data[:-1])
-        result = c.fetchone()
-        if result == None:
-            create_pages(c,data)
-        else:
-            print(result)
-            update_pages(c, result['id'])
+def update_or_create_page(c,data):
+    sql = "SELECT * FROM pages where name=? and session=?"
+    c.execute(sql,data[:-1])
+    result = c.fetchone()
+    if result == None:
+        create_pages(c,data)
+    else:
+        print(result)
+        update_pages(c, result['id'])
 
-    def create_pages(c, data):
-        print(data)
-        sql = ''' INSERT INTO pages(name,session,first_visited)
+def create_pages(c, data):
+    print(data)
+    sql = ''' INSERT INTO pages(name,session,first_visited)
                   VALUES (?,?,?) '''
-        c.execute(sql, data)
+    c.execute(sql, data)
 
-    def update_pages(c, pageId):
-        print(pageId)
-        sql = ''' UPDATE pages
+def update_pages(c, pageId):
+    print(pageId)
+    sql = ''' UPDATE pages
                   SET visits = visits+1
                   WHERE id = ?'''
-        c.execute(sql, [pageId])
+    c.execute(sql, [pageId])
 
-    def create_session(c, data):
-        sql = ''' INSERT INTO sessions(ip, continent, country, city, os, browser, session, created_at)
+def create_session(c, data):
+    sql = ''' INSERT INTO sessions(ip, continent, country, city, os, browser, session, created_at)
                   VALUES (?,?,?,?,?,?,?,?) '''
-        c.execute(sql, data)
+    c.execute(sql, data)
 
-    def select_all_sessions(c):
-        sql = "SELECT * FROM sessions"
-        c.execute(sql)
-        rows = c.fetchall()
-        return rows
+def select_all_sessions(c):
+    sql = "SELECT * FROM sessions"
+    c.execute(sql)
+    rows = c.fetchall()
+    return rows
 
-    def select_all_pages(c):
-        sql = "SELECT * FROM pages"
-        c.execute(sql)
-        rows = c.fetchall()
-        return rows
+def select_all_pages(c):
+    sql = "SELECT * FROM pages"
+    c.execute(sql)
+    rows = c.fetchall()
+    return rows
 
     def select_all_user_visits(c, session_id):
         sql = "SELECT * FROM pages where session =?"
